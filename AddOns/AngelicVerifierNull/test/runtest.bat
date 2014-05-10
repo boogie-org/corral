26651cfc0dc6e9acd7c08da6b21969fe7cd2d3ca
Shuvendu Lahiri <shuvendu@microsoft.com>
2014-05-10 01:15:43 -0700

    revert test file

--- AddOns/AngelicVerifierNull/test/runtest.bat
+++ AddOns/AngelicVerifierNull/test/runtest.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set BGEXE=..\AngelicVerifierNull\bin\release\AngelicVerifierNull.exe
+set BGEXE=..\AngelicVerifierNull\bin\debug\AngelicVerifierNull.exe
 
 for %%f in (t1.bpl t2_err.bpl t3_err.bpl t4.bpl t5.bpl t6.bpl t7.bpl t8.bpl pruned.bpl empty_forall.bpl) do (
   echo.
