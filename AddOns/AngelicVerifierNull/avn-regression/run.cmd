ca7894839b50821052dba38e00a1773c2e2f096b
Yi Li <t-liyi@microsoft.com>
2014-08-11 14:17:19 +0530

    Update scripts for regression.

--- AddOns/AngelicVerifierNull/avn-regression/run.cmd
+++ AddOns/AngelicVerifierNull/avn-regression/run.cmd
@@ -14,12 +14,14 @@ pushd %B%
 rem copy latest AVN binary from Yi's machine
 if not exist avn-bin (
 	mkdir avn-bin
-	copy \\tsclient\C\Users\t-liyi\Documents\repo\corral\AddOns\AngelicVerifierNull\AngelicVerifierNull\bin\Debug avn-bin
+	xcopy /D \\tsclient\C\Users\t-liyi\Documents\repo\corral\AddOns\AngelicVerifierNull\AngelicVerifierNull\bin\Debug avn-bin
 	rem copy \\tsclient\C\Users\t-liyi\Documents\repo\corral\AddOns\AngelicVerifierNull\AngelicVerifierNull\bin\Debug\*.dll avn-bin
 	rem copy \\tsclient\C\Users\t-liyi\Documents\repo\corral\AddOns\AngelicVerifierNull\AngelicVerifierNull\bin\Debug\z3.exe avn-bin
 )
-copy \\tsclient\C\Users\t-liyi\Documents\repo\corral\AddOns\AngelicVerifierNull\AngelicVerifierNull\bin\Debug\AngelicVerifierNull.exe avn-bin\AngelicVerifierNull.exe
 
-..\brunch.py --bench ..\drivers --threads 16 --format 'Status:Cpu:TotalTime(ms):Base:#Procs:#EntryPoints:#AssertsBeforeAA:#AssertsAfterAA:InstrumentTime(ms):alias.analysis:run.corral:run.corral.iterative:corral.count' -- avn-bin\AngelicVerifierNull.exe {f} /sdv /useEntryPoints /timeout:2000 /timeoutRoundRobin:60 /dumpResults:results.txt
+rem ..\brunch.py --bench ..\drivers --threads 8 --format 'Status:Base:Cpu:#Procs:#EntryPoints:#AssertsBeforeAA:#AssertsAfterAA:InstrumentTime(ms):blocked.count:alias.analysis(s):explain.error(s):run.corral(s):run.corral.iterative(s):corral.count:bug.count' -- avn-bin\AngelicVerifierNull.exe {f} /sdv /useEntryPoints /timeout:4000 /copt:newStratifiedInlining /copt:recursionBound:1 /copt:fwdBck /copt:v:1 /dumpResults:results.csv
+
+..\brunch.py --bench ..\drivers --threads 6 --format 'Status:Base:Result:Cpu:#Procs:#EntryPoints:#AssertsBeforeAA:#AssertsAfterAA:#AssertsAftHoudini:#ImplWithAsserts:InstrumentTime(ms):houdini(s):alias.analysis(s):explain.error(s):run.corral(s):run.corral.iterative(s):corral.count:blocked.count:bug.count' -- avn-bin\AngelicVerifierNull.exe {f} /sdv /useEntryPoints /timeoutRoundRobin:30 /timeout:3600 /houdini /dumpResults:results.csv
+
 echo Finished!
 popd
