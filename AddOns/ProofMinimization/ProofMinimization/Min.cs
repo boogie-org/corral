f711968fad2827908072caceb982da6776a90b90
akashlal <akashl@AKASHL-LT.fareast.corp.microsoft.com>
2015-07-25 07:46:07 +0530

    Added missing failes

---
+++ AddOns/ProofMinimization/ProofMinimization/Min.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Boogie;
+using System.Diagnostics;
+using cba.Util;
+using PersistentProgram = ProgTransformation.PersistentProgram;
+using System.Text.RegularExpressions;
+using btype = Microsoft.Boogie.Type;
+
+namespace ProofMinimization
+{
+    static class Minimize
+    {
+        static bool dbg = false;
+        static readonly int TemplateCounterStart = 1000;
+        static int IterCnt = 0;
+        public static bool usePerf = false;
+
+        // file name -> Program
+        static Dictionary<string, PersistentProgram> fileToProg = new Dictionary<string, PersistentProgram>();
+        // template ID -> file -> {constants}
+        static Dictionary<int, Dictionary<string, HashSet<string>>> templateMap = new Dictionary<int, Dictionary<string, HashSet<string>>>();
+        // template ID -> identifier string
+        static Dictionary<string, int> strToTemplate = new Dictionary<string, int>();
+        // file -> constants to keep always
+        static Dictionary<string, HashSet<string>> fileToKeepConstants = new Dictionary<string, HashSet<string>>();
+
+        public static void ReadFiles(IEnumerable<string> files, HashSet<string> keepPatterns)
+        {
+            var addTemplate = new Action<int, string, string>((templateId, file, constant) =>
+                {
+                    if (!templateMap.ContainsKey(templateId)) templateMap.Add(templateId, new Dictionary<string, HashSet<string>>());
+                    if (!templateMap[templateId].ContainsKey(file)) templateMap[templateId].Add(f, new HashSet<string>());
+                    templateMap[templateId][file].Add(constant);
+                });
+
+            foreach (var f in files)
+            {
+                var program = BoogieUtil.ReadAndResolve(f);
+                CheckRMT(program);
+
+                fileToKeepConstants.Add(f, new HashSet<string>());
+
+                // Add annotations
+                foreach (var p in keepPatterns)
+                {
+                    var re = new Regex(p);
+                    program.TopLevelDeclarations.OfType<Constant>()
+                    .Where(c => QKeyValue.FindBoolAttribute(c.Attributes, "existential") && re.IsMatch(c.Name))
+                    .Iter(c =>
+                        {
+                            c.AddAttribute(Driver.MustKeepAttr);
+                            fileToKeepConstants[f].Add(c.Name);
+                        });
+                }
+
+                // Prune, according to annotations
+                DropConstants(program, new HashSet<string>(
+                    program.TopLevelDeclarations.OfType<Constant>()
+                    .Where(c => QKeyValue.FindBoolAttribute(c.Attributes, Driver.DropAttr))
+                    .Select(c => c.Name)));
+
+                var allconstants = new Dictionary<string, Constant>();
+                program.TopLevelDeclarations.OfType<Constant>()
+                    .Where(c => QKeyValue.FindBoolAttribute(c.Attributes, "existential"))
+                    .Iter(c => allconstants.Add(c.Name, c));
+
+                // Normalize expressions
+                program.TopLevelDeclarations.OfType<Implementation>()
+                    .Iter(impl => impl.Proc.Ensures.Iter(ens => SimplifyExpr.SimplifyEnsures(ens, allconstants)));
+
+                // Remove constants that don't hold -- optimization
+
+
+
+                // create template map
+                foreach (var impl in program.TopLevelDeclarations.OfType<Implementation>())
+                {
+                    foreach (var ens in impl.Proc.Ensures.Where(e => !e.Free))
+                    {
+                        string constantName = null;
+                        Expr expr = null;
+
+                        var match = Microsoft.Boogie.Houdini.Houdini.GetCandidateWithoutConstant(
+                            ens.Condition, allconstants.Keys, out constantName, out expr);
+
+                        if (!match) continue;
+                        if (fileToKeepConstants[f].Contains(constantName)) continue;
+
+                        var templateId = QKeyValue.FindIntAttribute(ens.Attributes, "template", -1);
+                        if (templateId != -1) addTemplate(templateId, f, constantName);
+                        else
+                        {
+                            var temp = SimplifyExpr.ExprToTemplate(expr);
+                            if (strToTemplate.ContainsKey(temp))
+                            {
+                                // template for it exists
+                                addTemplate(strToTemplate[temp], f, constantName);
+                            }
+                            else
+                            {
+                                // create new template
+                                strToTemplate.Add(temp, TemplateCounterStart + strToTemplate.Count);
+                                addTemplate(strToTemplate[temp], f, constantName);
+                            }
+                        }
+                    }
+
+                }
+
+
+
+
+                // stash the program
+                fileToProg.Add(f, new PersistentProgram(program));
+            }
+
+            if (dbg)
+            {
+                var id2str = new Dictionary<int, string>();
+                strToTemplate.Iter(tup => id2str.Add(tup.Value, tup.Key));
+
+                foreach (var tup in templateMap)
+                {
+                    Console.WriteLine("Template {0} :: {1}", tup.Key, id2str[tup.Key]);
+                    foreach (var tup2 in tup.Value)
+                    {
+                        Console.WriteLine("  File {0}", tup2.Key);
+                        tup2.Value.Iter(c => Console.WriteLine("    Candidate {0}", c));
+                    }
+                }
+            }
+        }
+
+        // Prune away non-candidates, verify using the rest
+        static BoogieVerify.ReturnStatus PruneAndRun(HashSet<int> candidateTemplates, ref int inlined)
+        {
+            foreach (var tup in fileToProg)
+            {
+                var file = tup.Key;
+
+                var program = tup.Value.getProgram();
+                program.Typecheck();
+
+                // Gather the candidates
+            }
+
+
+            // Remove non-candidates
+            CoreLib.HoudiniInlining.InstrumentHoudiniAssignment(program, candidates, true);
+            program.RemoveTopLevelDeclarations(decl => (decl is Constant) && QKeyValue.FindBoolAttribute(decl.Attributes, "existential") &&
+                !candidates.Contains((decl as Constant).Name));
+
+            //BoogieUtil.PrintProgram(program, "hi_query" + IterCnt + ".bpl");
+
+            // Run Houdini
+            assignment = CoreLib.HoudiniInlining.RunHoudini(program, true);
+            //Console.WriteLine("  >> Contracts: {0}", assignment.Count);
+
+            // Read the program again, add contracts
+            program = inp.getProgram();
+            program.Typecheck();
+
+            CoreLib.HoudiniInlining.InstrumentHoudiniAssignment(program, assignment);
+            //BoogieUtil.PrintProgram(program, "si_query" + IterCnt + ".bpl");
+
+            // Run SI
+            var err = new List<BoogieErrorTrace>();
+
+            // Set bound
+            BoogieVerify.options.maxInlinedBound = 0;
+            if (inlined != 0)
+                BoogieVerify.options.maxInlinedBound = PerfMetric(inlined);
+
+            var rstatus = BoogieVerify.Verify(program, out err, true);
+            //Console.WriteLine(string.Format("  >> Procedures Inlined: {0}", BoogieVerify.CallTreeSize));
+            //Console.WriteLine(string.Format("Boogie verification time: {0} s", BoogieVerify.verificationTime.TotalSeconds.ToString("F2")));
+
+            inlined = BoogieVerify.CallTreeSize + 1;
+            BoogieVerify.options.CallTree = new HashSet<string>();
+            BoogieVerify.CallTreeSize = 0;
+            BoogieVerify.verificationTime = TimeSpan.Zero;
+
+
+            return rstatus;
+        }
+
+
+        // Prune away non-candidates, verify using the rest
+        static BoogieVerify.ReturnStatus PruneAndRun(PersistentProgram inp, HashSet<string> candidates, out HashSet<string> assignment, ref int inlined)
+        {
+            IterCnt++;
+
+            var program = inp.getProgram();
+            program.Typecheck();
+
+            // Remove non-candidates
+            CoreLib.HoudiniInlining.InstrumentHoudiniAssignment(program, candidates, true);
+            program.RemoveTopLevelDeclarations(decl => (decl is Constant) && QKeyValue.FindBoolAttribute(decl.Attributes, "existential") &&
+                !candidates.Contains((decl as Constant).Name));
+
+            //BoogieUtil.PrintProgram(program, "hi_query" + IterCnt + ".bpl");
+
+            // Run Houdini
+            assignment = CoreLib.HoudiniInlining.RunHoudini(program, true);
+            //Console.WriteLine("  >> Contracts: {0}", assignment.Count);
+
+            // Read the program again, add contracts
+            program = inp.getProgram();
+            program.Typecheck();
+
+            CoreLib.HoudiniInlining.InstrumentHoudiniAssignment(program, assignment);
+            //BoogieUtil.PrintProgram(program, "si_query" + IterCnt + ".bpl");
+
+            // Run SI
+            var err = new List<BoogieErrorTrace>();
+
+            // Set bound
+            BoogieVerify.options.maxInlinedBound = 0;
+            if (inlined != 0)
+                BoogieVerify.options.maxInlinedBound = PerfMetric(inlined);
+
+            var rstatus = BoogieVerify.Verify(program, out err, true);
+            //Console.WriteLine(string.Format("  >> Procedures Inlined: {0}", BoogieVerify.CallTreeSize));
+            //Console.WriteLine(string.Format("Boogie verification time: {0} s", BoogieVerify.verificationTime.TotalSeconds.ToString("F2")));
+
+            inlined = BoogieVerify.CallTreeSize + 1;
+            BoogieVerify.options.CallTree = new HashSet<string>();
+            BoogieVerify.CallTreeSize = 0;
+            BoogieVerify.verificationTime = TimeSpan.Zero;
+
+
+            return rstatus;
+        }
+
+        static int PerfMetric(int n)
+        {
+            if (!usePerf) return Int32.MaxValue;
+            if (n < 50) return (n + 100);
+            return 2 * n;
+        }
+
+        static Random rand = null;
+
+        // pick one with max cost randomly
+        string PickRandom(HashSet<string> set)
+        {
+            Debug.Assert(set.Count != 0);
+
+            if (rand == null)
+            {
+                rand = new Random((int)DateTime.Now.Ticks);
+            }
+
+            //var max = set.Select(c => candidateToCost[c]).Max();
+            //var selected = set.Where(s => candidateToCost[s] == max);
+            var selected = set;
+
+            var ind = rand.Next(selected.Count());
+            return selected.ToList()[ind];
+        }
+
+        // Prune away some constants according to annotations
+        static void DropConstants(Program program, HashSet<string> drop)
+        {
+            
+            var constants = new HashSet<string>(
+                program.TopLevelDeclarations.OfType<Constant>()
+                .Select(c => c.Name));
+
+            var dontdrop = constants.Difference(drop);
+
+            CoreLib.HoudiniInlining.InstrumentHoudiniAssignment(program, dontdrop, true);
+            program.RemoveTopLevelDeclarations(c => c is Constant && drop.Contains((c as Constant).Name));
+        }
+
+        // Input program must be an RMT query
+        static void CheckRMT(Program program)
+        {
+            if (program.TopLevelDeclarations.OfType<Implementation>()
+                .Any(impl => impl.Blocks
+                    .Any(blk => blk.Cmds
+                        .Any(c => c is AssertCmd && !BoogieUtil.isAssertTrue(c)))))
+                throw new Exception("Input program cannot have an assertion");
+
+            var ep = program.TopLevelDeclarations.OfType<Implementation>()
+                .Where(impl => QKeyValue.FindBoolAttribute(impl.Attributes, "entrypoint"))
+                .FirstOrDefault();
+
+            if (ep == null)
+                throw new Exception("Entrypoint not found");
+        }
+    }
+}
---
+++ AddOns/ProofMinimization/ProofMinimization/SimplifyExpr.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Boogie;
+using System.Diagnostics;
+using cba.Util;
+using PersistentProgram = ProgTransformation.PersistentProgram;
+using System.Text.RegularExpressions;
+using btype = Microsoft.Boogie.Type;
+
+namespace ProofMinimization
+{
+    // remove implications, push negation inside
+    public class SimplifyExpr : StandardVisitor
+    {
+        private SimplifyExpr() { }
+
+        public static void SimplifyEnsures(Ensures ens, Dictionary<string, Constant> CandidateConstants)
+        {
+            if (ens.Free) return;
+
+            string constantName = null;
+            Expr expr = null;
+
+            var match = Microsoft.Boogie.Houdini.Houdini.GetCandidateWithoutConstant(
+                ens.Condition, CandidateConstants.Keys, out constantName, out expr);
+
+            if (!match) return;
+
+            ens.Condition = Expr.Imp(Expr.Ident(CandidateConstants[constantName]), Simplify(expr));
+        }
+
+        public static Expr Simplify(Expr e)
+        {
+            var vs = new SimplifyExpr();
+            var e1 = vs.VisitExpr(e);
+            var e2 = NormalizeExpr(e1);
+            return e2;
+        }
+
+        public override Expr VisitNAryExpr(NAryExpr node)
+        {
+            Expr ret = node;
+
+            // Remove implies
+            if (node.Fun is BinaryOperator && (node.Fun as BinaryOperator).Op == BinaryOperator.Opcode.Imp)
+            {
+                ret = Expr.Or(Expr.Not(node.Args[0]), node.Args[1]) as NAryExpr;
+            }
+
+            // Push negation inside
+            if (node.Fun is UnaryOperator && (node.Fun as UnaryOperator).Op == UnaryOperator.Opcode.Not)
+            {
+                ret = PushNegationInside(node.Args[0]);
+            }
+
+            return base.VisitExpr(ret);
+        }
+
+
+        // push negations inside
+        static Expr PushNegationInside(Expr expr)
+        {
+            var nary = expr as NAryExpr;
+            if (nary == null)
+                return Expr.Not(expr);
+
+            if (nary.Fun is UnaryOperator && (nary.Fun as UnaryOperator).Op == UnaryOperator.Opcode.Not)
+                return nary.Args[0];
+
+            if (nary.Fun is BinaryOperator && (nary.Fun as BinaryOperator).Op == BinaryOperator.Opcode.And)
+                return Expr.Or(PushNegationInside(nary.Args[0]), PushNegationInside(nary.Args[1]));
+
+            if (nary.Fun is BinaryOperator && (nary.Fun as BinaryOperator).Op == BinaryOperator.Opcode.Or)
+                return Expr.And(PushNegationInside(nary.Args[0]), PushNegationInside(nary.Args[1]));
+
+            return Expr.Not(expr);
+        }
+
+        // replace formal-ins and outs with a unique variable
+        public static string ExprToTemplate(Expr expr)
+        {
+            var GetFin = new Func<btype, Variable>(ty =>
+                BoogieAstFactory.MkFormal("v_fin_" + ty.ToString(), ty, true));
+            var GetFout = new Func<btype, Variable>(ty =>
+                BoogieAstFactory.MkFormal("v_fout_" + ty.ToString(), ty, true));
+
+            var ret =
+                Substituter.Apply(new Substitution(v =>
+                {
+                    if (v is Formal && (v as Formal).InComing)
+                        return Expr.Ident(GetFin(v.TypedIdent.Type));
+                    if (v is Formal && !(v as Formal).InComing)
+                        return Expr.Ident(GetFout(v.TypedIdent.Type));
+                    return Expr.Ident(v);
+                }), expr);
+
+            return ret.ToString();
+        }
+
+        static Expr NormalizeExpr(Expr expr)
+        {
+            var disj = GetExprDisjuncts(expr);
+            if (disj.Count != 1)
+            {
+                disj = disj.Map(e => NormalizeExpr(e));
+                disj.Sort(new Comparison<Expr>((e1, e2) => (e1.ToString().CompareTo(e2.ToString()))));
+                var ret = disj[0];
+                for (int i = 1; i < disj.Count; i++)
+                    ret = Expr.Or(ret, disj[i]);
+                return ret;
+            }
+
+            var conj = GetExprConjunctions(expr);
+            if (conj.Count != 1)
+            {
+                conj = conj.Map(e => NormalizeExpr(e));
+                conj.Sort(new Comparison<Expr>((e1, e2) => (e1.ToString().CompareTo(e2.ToString()))));
+
+                return Expr.And(conj);
+            }
+
+            var naryexpr = expr as NAryExpr;
+            if (naryexpr == null)
+                return expr;
+
+            if (naryexpr.Fun is BinaryOperator)
+            {
+                NAryExpr nexpr;
+                bool applyNeg;
+
+                NormalizeOperator((naryexpr.Fun as BinaryOperator), naryexpr.Args[0], naryexpr.Args[1], out nexpr, out applyNeg);
+
+                if (applyNeg) return Expr.Not(nexpr);
+                return nexpr;
+            }
+
+            return expr;
+        }
+
+        static void NormalizeOperator(BinaryOperator op, Expr arg1, Expr arg2, out NAryExpr expr, out bool applyNeg)
+        {
+            expr = null;
+            applyNeg = false;
+
+            BinaryOperator.Opcode newop = op.Op;
+
+            if (op.Op == BinaryOperator.Opcode.Lt)
+                newop = BinaryOperator.Opcode.Ge;
+            else if (op.Op == BinaryOperator.Opcode.Le)
+                newop = BinaryOperator.Opcode.Gt;
+            else if (op.Op == BinaryOperator.Opcode.Neq)
+                newop = BinaryOperator.Opcode.Eq;
+
+            var comp = new Comparison<Expr>((e1, e2) => (e1.ToString().CompareTo(e2.ToString())));
+            var constructExpr = new Func<BinaryOperator, Expr, Expr, NAryExpr>((o, e1, e2) =>
+            {
+                if (o.Op == BinaryOperator.Opcode.Eq && comp(e1, e2) > 0)
+                    return new NAryExpr(Token.NoToken, o, new List<Expr> { e2, e1 });
+                return new NAryExpr(Token.NoToken, o, new List<Expr> { e1, e2 });
+            });
+
+            if (newop == op.Op)
+            {
+                expr = constructExpr(op, arg1, arg2);
+                applyNeg = false;
+            }
+
+            expr = constructExpr(new BinaryOperator(Token.NoToken, newop), arg1, arg2);
+            applyNeg = true;
+        }
+
+
+        public static List<Expr> GetExprConjunctions(Expr expr)
+        {
+            return GetSubExprs(expr, BinaryOperator.Opcode.And);
+        }
+
+        public static List<Expr> GetExprDisjuncts(Expr expr)
+        {
+            return GetSubExprs(expr, BinaryOperator.Opcode.Or);
+        }
+
+        // Return the set of conjuncts of the expr
+        public static List<Expr> GetSubExprs(Expr expr, BinaryOperator.Opcode op)
+        {
+            var conjuncts = new List<Expr>();
+            if (expr is NAryExpr && (expr as NAryExpr).Fun is BinaryOperator &&
+                ((expr as NAryExpr).Fun as BinaryOperator).Op == op)
+            {
+                var c0 = GetSubExprs((expr as NAryExpr).Args[0], op);
+                var c1 = GetSubExprs((expr as NAryExpr).Args[1], op);
+                conjuncts.AddRange(c0);
+                conjuncts.AddRange(c1);
+            }
+            else
+            {
+                conjuncts.Add(expr);
+            }
+
+            return conjuncts;
+        }
+
+        // Break (a && b) || (c && d) to {a, b, c, d}
+        public static List<Expr> BreakDownExpr(Expr expr)
+        {
+            var ret = new List<Expr>();
+            var disj = GetExprDisjuncts(expr);
+            disj.Iter(d => ret.AddRange(GetExprConjunctions(d)));
+            return ret;
+        }
+
+    }
+}
