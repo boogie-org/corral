310c59c8845ae5d3e403f483eec1d3afe9ab1d51
Shuvendu Lahiri <shuvendu.lahiri@gmail.com>
2017-08-12 17:21:46 -0700

    AV: updating chakra scripts

--- AddOns/AngelicVerifierNull/test/chakra-bpl/smv-av-scripts/update-smv-installation.cmd
+++ AddOns/AngelicVerifierNull/test/chakra-bpl/smv-av-scripts/update-smv-installation.cmd
@@ -1,5 +1,5 @@
-xcopy smv.cmd %SDV%\smv.cmd 
-xcopy chakrachecks-nobuild-sf-cloud.xml %SDV%\smv\analysisplugins\av\configurations\
-xcopy chakra-type-confusion-annots.txt %SDV%\smv\analysisplugins\av\bin\engine\exampleProperties\
-xcopy type-confusion.avp %SDV%\smv\analysisplugins\av\bin\engine\exampleProperties\
-xcopy ..\..\..\..\fastavn\fastavn\bin\debug\* %SDV%\smv\analysisplugins\av\bin\engine\
+xcopy /Y smv.cmd %SDV%\smv.cmd 
+xcopy /Y chakrachecks-nobuild-sf-cloud.xml %SDV%\smv\analysisplugins\av\configurations\
+xcopy /Y chakra-type-confusion-annots.txt %SDV%\smv\analysisplugins\av\bin\engine\exampleProperties\
+xcopy /Y type-confusion.avp %SDV%\smv\analysisplugins\av\bin\engine\exampleProperties\
+xcopy /Y ..\..\..\..\fastavn\fastavn\bin\debug\* %SDV%\smv\analysisplugins\av\bin\engine\
