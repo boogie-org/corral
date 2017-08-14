47bcba30a8893506ed24ca1251ca28b08ca36f95
Shuvendu Lahiri <shuvendu.lahiri@gmail.com>
2017-08-14 15:47:34 -0700

    AV: tweaking chakra scripts

--- AddOns/AngelicVerifierNull/test/chakra-bpl/smv-av-scripts/update-smv-installation.cmd
+++
@@ -1,5 +0,0 @@
-xcopy /Y smv.cmd %SDV%\smv.cmd 
-xcopy /Y chakrachecks-nobuild-sf-cloud.xml %SDV%\smv\analysisplugins\av\configurations\
-xcopy /Y chakra-type-confusion-annots.txt %SDV%\smv\analysisplugins\av\bin\engine\exampleProperties\
-xcopy /Y type-confusion.avp %SDV%\smv\analysisplugins\av\bin\engine\exampleProperties\
-xcopy /Y ..\..\..\..\fastavn\fastavn\bin\debug\* %SDV%\smv\analysisplugins\av\bin\engine\
