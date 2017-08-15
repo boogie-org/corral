59585a4f77da165cae403f44a16baa43aa36fb8b
Shuvendu Lahiri <shuvendu.lahiri@gmail.com>
2017-08-14 17:27:54 -0700

    AV: fixes in avp file

--- AddOns/AngelicVerifierNull/test/chakra-bpl/update-smv-installation.cmd
+++ AddOns/AngelicVerifierNull/test/chakra-bpl/update-smv-installation.cmd
@@ -1,5 +1,7 @@
 xcopy /Y smv-av-scripts\smv.cmd %SDV%\smv.cmd 
 xcopy /Y smv-av-scripts\chakrachecks-nobuild-sf-cloud.xml %SDV%\smv\analysisplugins\av\configurations\
 xcopy /Y smv-av-scripts\chakra-type-confusion-annots.txt %SDV%\smv\analysisplugins\av\bin\engine\exampleProperties\
-xcopy /Y smv-av-scripts\ type-confusion.avp %SDV%\smv\analysisplugins\av\bin\engine\exampleProperties\
+xcopy /Y smv-av-scripts\type-confusion.avp %SDV%\smv\analysisplugins\av\bin\engine\exampleProperties\
+xcopy /Y ..\..\..\fastavn\fastavn\bin\debug\SmackInst.exe %SDV%\smv\analysisplugins\av\bin\engine\
+xcopy /Y ..\..\..\fastavn\fastavn\bin\debug\PropInst.exe %SDV%\smv\analysisplugins\av\bin\engine\
 REM xcopy /Y ..\..\..\fastavn\fastavn\bin\debug\* %SDV%\smv\analysisplugins\av\bin\engine\
